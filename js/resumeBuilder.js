2ff21e81f0b7f8750b02353ba8ca480c5ff90855
jennyweegar <jennyweegar@yahoo.com>
2017-01-03 10:41:24 -0500

    Quiz: Format Data, Build A Resume Header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var HTMLheaderName = "<h1 id='name'>%data%</h1>";
+var HTMLheaderRole = "<span>%data%</span><hr/>";
+
+var name = "Jenni Weegar";
+var formattedName = HTMLheaderName.replace (%data%, name); 
+$("#header").append(formattedName);
+
+var role = "Web Developer";
+var formattedRole =HTMLheaderRole.replace(%data%, role);
+$("#header").append(formattedRole);
